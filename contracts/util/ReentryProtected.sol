e1d26f2092f8a16b22e91db954177adae63d8d0a
plouw <philip@becodemonkey.com>
2017-10-18 21:17:08 +0100

    comment changes

--- contracts/util/ReentryProtected.sol
+++ contracts/util/ReentryProtected.sol
@@ -1,20 +1,22 @@
 pragma solidity ^0.4.18;
 
 contract ReentryProtected {
-    // The reentry protection state mutex.
-    bool __reMutex;
 
-    // Sets and resets mutex in order to block functin reentry
-    modifier preventReentry() {
-        require(!__reMutex);
-        __reMutex = true;
-        _;
-        delete __reMutex;
-    }
+  // The reentry protection state mutex.
+  bool private rentrancy_lock = false;
+  
+  // Sets and resets mutex in order to block functin reentry
+  modifier preventReentry() {
+    require(!rentrancy_lock);
+    rentrancy_lock = true;
+    _;
+    delete rentrancy_lock;
+    return;
+  }
 
-    // Blocks function entry if mutex is set
-    modifier noReentry() {
-        require(!__reMutex);
-        _;
-    }
+  // Blocks function entry if mutex is set
+  modifier noReentry() {
+    require(!rentrancy_lock);
+    _;
+  }
 }
